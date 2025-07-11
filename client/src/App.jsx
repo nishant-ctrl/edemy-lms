b8fec7f171c116f6901afb5281a3afba3a58f621
nishant-ctrl <knishant9135@gmail.com>
2025-07-12 00:04:08 +0530

    completed navbar and integrated clerk auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,37 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useMatch } from "react-router-dom";
 import {CourseDetails, CoursesList, Home, MyEnrollments, Player} from "./pages/student"
 import {AddCouse, Dashboard, Educator, MyCourses, StudentsEnrolled} from "./pages/educator"
-import {Loading} from "./components/student"
+import {Loading, Navbar} from "./components/student"
 
 function App() {
-    return <div>
-        <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/course-list" element={<CoursesList />}/>
-            <Route path="/course-list/:input" element={<CoursesList />}/>
-            <Route path="/course/:id" element={<CourseDetails />}/>
-            <Route path="/my-enrollments" element={<MyEnrollments />}/>
-            <Route path="/player/:courseId" element={<Player />}/>
-            <Route path="/loading/:path" element={<Loading />}/>
-            <Route path="/educator" element={<Educator />}>
-                <Route path="" element={<Dashboard />}/>
-                <Route path="add-course" element={<AddCouse />}/>
-                <Route path="my-courses" element={<MyCourses />}/>
-                <Route path="student-enrolled" element={<StudentsEnrolled />}/>
-            </Route>
-        </Routes>
-    </div>;
+
+    const isEducatorRoute=useMatch("/educator/")
+
+
+    return (
+        <div className="text-default min-h-scren bg-white">
+            {!isEducatorRoute && <Navbar />}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/course-list" element={<CoursesList />} />
+                <Route path="/course-list/:input" element={<CoursesList />} />
+                <Route path="/course/:id" element={<CourseDetails />} />
+                <Route path="/my-enrollments" element={<MyEnrollments />} />
+                <Route path="/player/:courseId" element={<Player />} />
+                <Route path="/loading/:path" element={<Loading />} />
+                <Route path="/educator" element={<Educator />}>
+                    <Route path="" element={<Dashboard />} />
+                    <Route path="add-course" element={<AddCouse />} />
+                    <Route path="my-courses" element={<MyCourses />} />
+                    <Route
+                        path="student-enrolled"
+                        element={<StudentsEnrolled />}
+                    />
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 function NavBar() {
-  return (
-    <div>NavBar</div>
-  )
+    const isCourseListPage = location.pathname.includes("/course-list");
+
+    const { openSignIn } = useClerk();
+    const { user } = useUser();
+
+    return (
+        <div
+            className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-r-gray-500 py-4 ${
+                isCourseListPage ? "bg-white" : "bg-cyan-100/70"
+            }`}
+        >
+            <img
+                src={assets.logo}
+                alt="Logo"
+                className="w-28 lg:w-32 cursor-pointer"
+            />
+            <div className="hidden md:flex items-center gap-5 text-gray-500">
+                <div className="flex items-center gap-5">
+                    {user && (
+                        <>
+                            <button>Become Educator</button> |
+                            <Link to="/my-enrollments">My Enrollments</Link>
+                        </>
+                    )}
+                </div>
+                {user ? (
+                    <UserButton />
+                ) : (
+                    <button
+                        className="bg-blue-600 text-white px-5 py-2 rounded-full"
+                        onClick={() => {
+                            openSignIn();
+                        }}
+                    >
+                        Create Account
+                    </button>
+                )}
+            </div>
+            <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
+                <div className="flex items-center gap-2 sm:gap-2 max-sm:text-xs">
+                    {user && (
+                        <>
+                            <button>Become Educator</button> |
+                            <Link to="/my-enrollments">My Enrollments</Link>
+                        </>
+                    )}
+                </div>
+                {user ? (
+                    <UserButton />
+                ) : (
+                    <button
+                        onClick={() => {
+                            openSignIn();
+                        }}
+                    >
+                        <img src={assets.user_icon} alt="" />
+                    </button>
+                )}
+            </div>
+        </div>
+    );
 }
 
-export default NavBar
+export default NavBar;
